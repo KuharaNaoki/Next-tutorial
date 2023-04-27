b88ed11c684ca9ca595d0094583c12a0b5bda699
kuhara.naoki <kuhara.naoki@ntc.co.jp>
2023-04-27 15:58:41 +0900

    [add]: 初めてのコミット

---
+++ components/layout.js
@@ -0,0 +1,88 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const name = 'Naoki Kuhara';
+export const siteTitle = 'Next.js Sample Website';
+
+export default function Layout({ children, home }) {
+  const router = useRouter();
+
+  const routerFirstPost = () => {
+    router.push({
+      pathname: '/posts/first-post',
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Learn how to build a personal website using Next.js'
+        />
+        <meta
+          property='og:image'
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src='/images/profile.jpg'
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=''
+            />
+            <h1
+              className={utilStyles.heading2Xl}
+              onClick={() => routerFirstPost()}
+            >
+              {name}
+            </h1>
+          </>
+        ) : (
+          <>
+            <Link href='/'>
+              <Image
+                priority
+                src='/images/profile.jpg'
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=''
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href='/' className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href='/'>← Use link : Back to home</Link>
+          <div>
+            <a className={styles.cursor} onClick={() => router.back()}>
+              ← Use useRouter : Back to home
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
